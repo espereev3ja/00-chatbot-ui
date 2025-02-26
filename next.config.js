fdd630470b232065a9b49f3cd6d50266d8d04c91
MARIA LUIZA BARBOSA <70168128+espereev3ja@users.noreply.github.com>
2025-02-26 16:37:32 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,32 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
+enabled: process.env.ANALYZE === "true"
+})
 
-module.exports = withBundleAnalyzer({
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      { protocol: "http", hostname: "localhost" },
-      { protocol: "http", hostname: "127.0.0.1" },
-      { protocol: "https", hostname: "**" },
-    ],
-  },
-  experimental: {
-    serverComponentsExternalPackages: ["sharp", "onnxruntime-node"],
-  },
-});
+const withPWA = require("next-pwa")({
+  dest: "public"
+})
+
+module.exports = withBundleAnalyzer(
+  withPWA({
+    reactStrictMode: true,
+    images: {
+      remotePatterns: [
+        {
+          protocol: "http",
+          hostname: "localhost"
+        },
+        {
+          protocol: "http",
+          hostname: "127.0.0.1"
+        },
+        {
+          protocol: "https",
+          hostname: "**"
+        }
+      ]
+    },
+    experimental: {
+      serverComponentsExternalPackages: ["sharp", "onnxruntime-node"]
+    }
+  })
+)
