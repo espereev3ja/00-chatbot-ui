dce25be1d328e69343118fb75bc02ff2cb01a458
MARIA LUIZA BARBOSA <70168128+espereev3ja@users.noreply.github.com>
2025-02-26 16:49:28 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 enabled: process.env.ANALYZE === "true"
 })
 
-const withPWA = require("next-pwa")({
-  dest: "public"
-})
+//const withPWA = require("next-pwa")({
+//  dest: "public"
+//})
 
 module.exports = withBundleAnalyzer(
   withPWA({
